69abba3cf3cb0090a32172514c73e9ba2c953a83
Deficuet <Deficuet@hotmail.com>
2022-12-28 12:32:53 -0700

    replace Path

--- src/main/kotlin/io/github/deficuet/tools/file/file.kt
+++ src/main/kotlin/io/github/deficuet/tools/file/file.kt
@@ -1,17 +1,20 @@
 package io.github.deficuet.tools.file
 
 import java.nio.file.Files
-import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.deleteExisting
+import kotlin.io.path.isDirectory
+import kotlin.io.path.isRegularFile
 
 fun deleteDirectory(pathString: String) {
-    val path = Path.of(pathString)
+    val path = Path(pathString)
     Files.newDirectoryStream(path).use { stream ->
         stream.forEach {
             when {
-                Files.isDirectory(it) -> deleteDirectory(it.toString())
-                Files.isRegularFile(it) -> Files.delete(it)
+                it.isDirectory() -> deleteDirectory(it.toString())
+                it.isRegularFile() -> it.deleteExisting()
             }
         }
     }
-    Files.delete(path)
+    path.deleteExisting()
 }
